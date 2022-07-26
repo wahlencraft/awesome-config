d1c4a1111541069b17288af08fb0e72f4494018d
Albin Wahlén <alegow8@gmail.com>
2022-07-26 18:55:00 +0200

    New wallpaper

--- themes/my-theme/theme.lua
+++ themes/my-theme/theme.lua
@@ -15,9 +15,11 @@ local liu_timeedit = require("liu-timeedit")
 local os = os
 local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
+
 local theme                                     = {}
+
 theme.confdir                                   = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
-theme.wallpaper                                 = theme.confdir .. "/wall.png"
+theme.wallpaper                                 = os.getenv("HOME") .. "/onedrive/Pictures/Wallpapers/webb1440.png"
 theme.font                                      = "Terminus 11"
 theme.menu_bg_normal                            = "#000000"
 theme.menu_bg_focus                             = "#000000"
